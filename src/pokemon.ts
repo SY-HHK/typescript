d1ad78b0b146457cfccef42d8f84730972eaa726
HHK <louis.ricour@gmail.com>
2021-04-30 11:44:03 +0200

    feat: create attack method & add health on pokemon class

--- src/pokemon.ts
+++ src/pokemon.ts
@@ -2,11 +2,23 @@ export class Pokemon {
     name: string;
     speed: number;
     damage: number;
+    health: number;
 
     constructor(name: string = 'pikachu', speed: number, damage: number) {
         this.name = name;
         this.speed = speed;
         this.damage = damage
+        this.health = 100;
+    }
+
+    attack(pokemon: Pokemon): number {
+        const attackDamage = (this.damage * Math.random()*10) / 20;
+        if (pokemon.health <= attackDamage) {
+            pokemon.health = 0;
+            return pokemon.health;
+        }
+        pokemon.health -= attackDamage;
+        return pokemon.health;
     }
 }
 
